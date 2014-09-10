3763a5eb616ddd7f029c0e34aba551e58d90d9f9
gpkfr <gpkfr@imelbox.com>
2014-09-10 23:12:15 +0200

    Added hhvm support to laravel manifest

--- manifests/laravel.pp
+++ manifests/laravel.pp
@@ -8,6 +8,7 @@ class {'laravel':
   use_xdebug      => false,
   remote_host_ip  => "10.0.0.69", # Needed for xdebug when using Vmware
   database_server => "none", # Possible value : none, mysql, postgresql,sqlite
+  use_hhvm        => true, # install hhvm
 }
 
 laravel::vhost { 'test.local':
