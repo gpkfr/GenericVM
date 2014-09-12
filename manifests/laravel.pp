3e98bc10aec59c217b41bc9bb67208c243a5a91e
gpkfr <gpkfr@imelbox.com>
2014-09-12 12:39:32 +0200

    Added beanstalkd support to laravel's manifest

--- manifests/laravel.pp
+++ manifests/laravel.pp
@@ -5,12 +5,14 @@ file { "/home/vagrant/www":
 }
 
 class {'laravel':
-  use_xdebug      => false, # install and configure xdebug ; If used with vmware_fusion uncomment remote_host_ip and put your host ip.
-  use_hhvm        => false, # install hhvm
-  #remote_host_ip => "10.0.0.69", # Needed for xdebug when using Vmware; delete or comment if you use virtualbox.
-  database_server => "none", # Possible value : none, mysql, postgresql,sqlite
-  install_node    => true,
-  npm_pkg         => ["gulp","bower","grunt"],
+  use_xdebug         => false, # install and configure xdebug ; If used with vmware_fusion uncomment remote_host_ip and put your host ip.
+  use_hhvm           => false, # install hhvm
+  #remote_host_ip    => "10.0.0.69", # Needed for xdebug when using Vmware; delete or comment if you use virtualbox.
+  database_server    => "none", # Possible value : none, mysql, postgresql,sqlite.
+  # When installed mysql and postgresql create an empty database "laravel" with user/pass == root/root
+  install_node       => true,
+  install_beanstalkd => false,
+  npm_pkg            => ["gulp","bower","grunt","grunt-cli"],
 }
 
 
@@ -19,3 +21,4 @@ laravel::vhost { 'test.local':
   root_dir => "/home/vagrant/www/public",
   require  => File["/home/vagrant/www"],
 }
+
