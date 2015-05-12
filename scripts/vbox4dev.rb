68b72be09e16020ad88b79a6f2c042d0ce5cfb44
Guillaume Pancak <gpkfr@imelbox.com>
2015-05-12 14:05:40 +0200

    Refacto

--- scripts/vbox4dev.rb
+++ scripts/vbox4dev.rb
@@ -12,32 +12,36 @@ class Vbox
 
     #Configure A private Network
     if "private" == settings["network_type"]
-      config.vm.network "private_network", ip: settings["ip"] ||= "192.168.212.230", auto_config: true
+      config.vm.network :private_network, ip: settings["ip"] ||= "192.168.212.230", auto_config: true
     end
 
     if "public" == settings["network_type"]
-      config.vm.network "public_network"
+      config.vm.network :public_network
     end
-    #config.vm.network "private_network", type: "dhcp"
 
     config.vm.hostname = "DevMachine"
 
-    # Configure A Few VmWare-Fusion Settings
-    config.vm.provider "vmware_fusion" do |v, override|
-      v.vmx["displayName"] = "DevMachine"
-      v.vmx["memsize"] = settings["memory"] ||= "1024"
-      v.vmx["numvcpus"] = settings["cpus"] ||= "1"
-      #v.vmx["ethernet0.connectiontype"] = settings["connectiontype"] ||= "nat"
-      #v.vmx["ethernet0.connectiontype"] = settings["connectiontype"] ||= "bridged"
-      #v.vmx["ethernet0.linkStatePropagation.enable"] = "TRUE"
+    # Configure A Few VmWare Settings
+    ["vmware_fusion", "vmware_workstation"].each do |vmware|
+      config.vm.provider vmware do |v, override|
+        v.vmx["displayName"] = "DevMachine"
+        v.vmx["memsize"] = settings["memory"] ||= "1024"
+        v.vmx["numvcpus"] = settings["cpus"] ||= "1"
+        v.vmx["guestos"] = settings["ostype"] || "debian7-64"
+        #v.vmx["ethernet0.connectiontype"] = settings["connectiontype"] ||= "nat"
+        #v.vmx["ethernet0.connectiontype"] = settings["connectiontype"] ||= "bridged"
+        #v.vmx["ethernet0.linkStatePropagation.enable"] = "TRUE"
+      end
     end
 
     # Configure A Few VirtualBox Settings
     config.vm.provider "virtualbox" do |vb|
+      vb.name = 'DevMachine'
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "1024"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      vb.customize ["modifyvm", :id, "--ostype", settings["ostype"] || "Debian-64"]
     end
 
 
@@ -49,28 +53,28 @@ class Vbox
     end
 
     # Configure The Public Key For SSH Access
-    if settings["authorized_keys"]
+    if settings.include 'authorized_keys'
       settings["authorized_keys"].each do |authkey|
         config.vm.provision "shell" do |s|
-          s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+          s.inline = "echo $1|grep -xq \"$1\" /home/vagrant/.ssh/authorized_keys||echo $1|tee -a /home/vagrant/.ssh/authorized_keys"
           s.args = [File.read(File.expand_path(authkey))]
         end
       end
     end
 
     # Copy The SSH Private Keys To The Box
-    if settings["private_keys"]
+    if settings.include 'private_keys'
       settings["private_keys"].each do |key|
         config.vm.provision "shell" do |s|
           s.privileged = false
-          s.inline = "echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
+          s.inline = "echo $1|grep -xq \"$1\" /home/vagrant/.ssh/$2||echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
           s.args = [File.read(File.expand_path(key)), key.split('/').last]
         end
       end
     end
 
     # Register All Of The Configured Shared Folders
-    if settings["shared_folders"]
+    if settings.include 'shared_folders'
       settings["shared_folders"].each do |folder|
         if folder["type"]
           config.vm.synced_folder folder["map"], folder["to"],id: folder["map"],
